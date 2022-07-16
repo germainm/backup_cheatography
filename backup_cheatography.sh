8b07a95a003681763ded6e15b150f785bd05f813
germainm <germainm@hotmail.com>
2022-07-16 19:43:51 -0400

    Create backup_cheatography.sh

---
+++ backup_cheatography.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+wayback_machine_downloader https://www.cheatography.com --only "/(latex)\/$/i" -c 4
+
+wayback_machine_downloader https://www.cheatography.com --only "/(pdf)\/$/i"
+
+wayback_machine_downloader http://www.cheatography.com --only "/\/cheat-sheets\/[a-z0-9\-_]+\/$/i" -c 4
